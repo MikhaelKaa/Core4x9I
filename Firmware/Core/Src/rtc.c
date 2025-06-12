0d72fd7adab444e9f2ec9691b57bb4601fa091a6
Mikhail.Kargapolcev <KargapolcevME@sinara-group.com>
2025-06-13 02:03:57 +0300

    fix: инит часов при старте (непроверенный)

--- Firmware/Core/Src/rtc.c
+++ Firmware/Core/Src/rtc.c
@@ -21,7 +21,7 @@
 #include "rtc.h"
 
 /* USER CODE BEGIN 0 */
-
+volatile static uint8_t BKUP_wr_flag;
 /* USER CODE END 0 */
 
 RTC_HandleTypeDef hrtc;
@@ -31,8 +31,13 @@ void MX_RTC_Init(void)
 {
 
   /* USER CODE BEGIN RTC_Init 0 */
-
+  if(HAL_RTCEx_BKUPRead(&hrtc, 7) != 0x0decaf0) {
+    BKUP_wr_flag = 1;
+  }
   /* USER CODE END RTC_Init 0 */
+
+  RTC_TimeTypeDef sTime = {0};
+  RTC_DateTypeDef sDate = {0};
 
   /* USER CODE BEGIN RTC_Init 1 */
 
@@ -51,8 +56,36 @@ void MX_RTC_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN RTC_Init 2 */
 
+  /* USER CODE BEGIN Check_RTC_BKUP */
+  if(BKUP_wr_flag) HAL_RTCEx_BKUPWrite(&hrtc, 7, 0x0decaf0);
+  if(HAL_RTCEx_BKUPRead(&hrtc, 7) == 0x0decaf0) {
+      return;
+  }
+  /* USER CODE END Check_RTC_BKUP */
+
+  /** Initialize RTC and set the Time and Date
+  */
+  sTime.Hours = 0;
+  sTime.Minutes = 0;
+  sTime.Seconds = 0;
+  sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
+  sTime.StoreOperation = RTC_STOREOPERATION_RESET;
+  if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sDate.WeekDay = RTC_WEEKDAY_MONDAY;
+  sDate.Month = RTC_MONTH_JANUARY;
+  sDate.Date = 1;
+  sDate.Year = 0;
+
+  if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BIN) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN RTC_Init 2 */
+  
   /* USER CODE END RTC_Init 2 */
 
 }
